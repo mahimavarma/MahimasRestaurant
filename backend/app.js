a016f576cdceebb493549e36b7af9bccc5060c62
mahimavarma <118841358+mahimavarma@users.noreply.github.com>
2024-06-24 20:02:48 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import mongoose from "mongoose";
+import mongoose from "mongoose";  // Ensure mongoose is imported
 import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 import { dbConnection } from "./database/dbConnection.js";
 
-const app = express();
-
-
 dotenv.config({ path: "./config/config.env" });
+
+const app = express();
 
 // CORS options
 const corsOptions = {
@@ -27,8 +26,11 @@ app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connect to the database
+dbConnection();
+
 // Example endpoint to test CORS
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   return res.status(200).json({
     success: true,
     message: "HELLO WORLD AGAIN"
@@ -41,7 +43,5 @@ app.use("/api/reservation", reservationRouter);
 // Apply error middleware
 app.use(errorMiddleware);
 
-// Connect to the database
-dbConnection();
-
 export default app;
+
