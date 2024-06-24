85745b9a7b069da017d93ce5a24300c476f7bb82
mahimavarma <118841358+mahimavarma@users.noreply.github.com>
2024-06-24 14:49:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ import { dbConnection } from "./database/dbConnection.js";
 const app = express();
 app.use(cors(
   {
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: ["https://mahimas-restaurant-api.vercel.app"],
     methods:["POST", "GET"],
     credentials: true
   }
