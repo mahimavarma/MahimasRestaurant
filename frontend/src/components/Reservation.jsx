ba2e9532ac4b7a9f35b3d66d23423c291cab3217
mahimavarma <118841358+mahimavarma@users.noreply.github.com>
2024-06-24 21:25:32 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://mahimas-restaurant-frontend.vercel.app",
+        "https://mahimas-restaurant-api.vercel.app/",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
